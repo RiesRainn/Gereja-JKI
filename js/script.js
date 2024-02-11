eb0e9ce689376525aaeaf82cece0d6b6541774bb
RiesRainn <150283403+RiesRainn@users.noreply.github.com>
2024-02-12 00:50:04 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 //toogle class
 
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 // klik menu
-document.querySelector('#hamburger-menu').onclick= () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
 // klik luar
-const hm = document.querySelector('#hamburger-menu');
+const hm = document.querySelector("#hamburger-menu");
 
-document.addEventListener('click', function(e){
-    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
-    navbarNav.classList.remove('active');
-    }
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
 
 // klik di luar modal
 window.onclick = (e) => {
   if (e.target === itemDetailModal) {
-    itemDetailModal.style.display = 'none';
+    itemDetailModal.style.display = "none";
   }
 };
+
